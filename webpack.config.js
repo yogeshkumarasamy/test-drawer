afc924ffa9073254e8daebb823d034a06836621d
yogesh <yogeshkumar_kumarasamy@trimble.com>
2021-04-26 22:36:53 +0530

    wp config changes

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,17 @@ module.exports = {
 
   module: {
     rules: [
-      // {
-      //   test: /\.(js|jsx)$/,
-      //   exclude: /node_modules/,
-      //   use: 'babel-loader'
-      // },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
